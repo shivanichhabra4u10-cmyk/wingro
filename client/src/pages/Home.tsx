585b33e95cf89d7390936de9ba6756ab2f0f9754
shichh <shivanichhabra4u10@gmail.com>
2025-11-30 22:54:03 -0800

    added brands images

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -91,7 +91,7 @@ const Home: React.FC = () => {
             {brandImages.map((id, index) => (
               <div key={index} className="flex-shrink-0" style={{ width: '180px', height: '180px', padding: '16px' }}>
                 <img
-                  src="/images/KiranMakkar.jpg"
+                  src={`/images/B${id}.jpg`}
                   alt={`Brand ${id}`}
                   className="w-full h-full object-contain"
                 />
